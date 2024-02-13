45eb9db0d97d52dc4511df52c7e9c774bb603aca
anastasiaelis <anastasia_elis@mail.ru>
2024-02-13 21:02:54 +0500

    7

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,10 +1,6 @@
 package ru.yandex.practicum.filmorate.model;
 
 import lombok.Data;
-
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.PastOrPresent;
 import java.time.LocalDate;
 
 @Data
