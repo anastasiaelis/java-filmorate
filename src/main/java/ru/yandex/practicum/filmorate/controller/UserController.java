7d35a58973a671fa65b98c81ed9ef3ce7adda336
anastasiaelis <anastasia_elis@mail.ru>
2024-02-18 20:00:34 +0500

    new branch

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -3,6 +3,8 @@ package ru.yandex.practicum.filmorate.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.FilmNotFoundException;
+import ru.yandex.practicum.filmorate.exception.UserNotFoundException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
 
@@ -24,6 +26,9 @@ public class UserController {
     public User findById(@PathVariable Integer id) {
         log.info("получен запрос получения пользователя по ID: {}", id);
         User user1 = userService.getUserById(id);
+        if (user1 == null) {
+            throw new UserNotFoundException("Не найден ID: " + id);
+        }
         log.info("получен пользователь: {}", user1);
         return user1;
     }
@@ -37,20 +42,19 @@ public class UserController {
     @PostMapping
     public User create(@Valid @RequestBody User user) {
         User user1 = userService.create(user);
-
-        if (!user.getEmail().contains("@")) {
+        if (!user1.getEmail().contains("@")) {
             log.error("Email пользователя пуст или не содержит @");
             throw new ValidationException("Email должен содержать @ и не быть пустым");
         }
-        if (user.getLogin().isBlank() || user.getLogin().contains(" ")) {
+        if (user1.getLogin().isBlank() || user.getLogin().contains(" ")) {
             log.error("Логин пользователя пуст или содержит пробелы");
             throw new ValidationException("Логин не должен быть пустым или содержать пробелы");
         }
-        if (user.getName() == null || user.getName().isBlank()) {
+        if (user1.getName() == null || user.getName().isBlank()) {
             user.setName(user.getLogin());
             log.info("Имя пользователя пустое, будет использован логин");
         }
-        if (user.getBirthday().isAfter(LocalDate.now())) {
+        if (user1.getBirthday().isAfter(LocalDate.now())) {
             log.error("Дата рождения пользователя в будущем");
             throw new ValidationException("Дата рождения не может быть в будущем");
         }
