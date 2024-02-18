24c73522e42f14f21c6ab9a4b941d66babc4715f
anastasiaelis <anastasia_elis@mail.ru>
2024-02-18 15:44:07 +0500

    new branch

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -16,36 +16,44 @@ public class FilmController {
     private final FilmService service;
     private final Map<Integer, Film> films = new HashMap<>();
     private int id = 0;
+
     @Autowired
     public FilmController(FilmService service) {
         this.service = service;
     }
+
     @PostMapping
     public Film create(@Valid @RequestBody Film film) {
         Validator.validateFilm(film);
         return service.create(film);
     }
+
     @PutMapping
     public Film update(@Valid @RequestBody Film film) {
         Validator.validateFilm(film);
         return service.update(film);
     }
+
     @GetMapping
     public List<Film> get() {
         return service.get();
     }
+
     @GetMapping("/{id}")
     public Film getFilmById(@PathVariable int id) {
         return service.getFilmById(id);
     }
+
     @PutMapping("/{id}/like/{userId}")
     public void addLike(@PathVariable("id") int filmId, @PathVariable long userId) {
         service.addLike(filmId, userId);
     }
+
     @DeleteMapping("/{id}/like/{userId}")
     public void removeLike(@PathVariable("id") int filmId, @PathVariable long userId) {
         service.removeLike(filmId, userId);
     }
+
     @GetMapping("/popular")
     public List<Film> getPopularMovies(@RequestParam(required = false, defaultValue = "10") int count) {
         return service.getPopularMovies(count);
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -5,44 +5,55 @@ import javax.validation.Valid;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import ru.yandex.practicum.filmorate.service.UserService;
+
 @RestController
+
 @RequestMapping("/users")
 public class UserController {
     private final UserService service;
+
     @Autowired
     public UserController(UserService service) {
         this.service = service;
     }
+
     @PostMapping
     public User create(@Valid @RequestBody User user) {
         Validator.validateUser(user);
         return service.create(user);
     }
+
     @PutMapping
     public User update(@Valid @RequestBody User user) {
         Validator.validateUser(user);
         return service.update(user);
     }
+
     @GetMapping
     public List<User> get() {
         return service.get();
     }
+
     @GetMapping("/{id}")
     public User getUserById(@PathVariable long id) {
         return service.getUserById(id);
     }
+
     @PutMapping("/{id}/friends/{friendId}")
     public void addFriend(@PathVariable long id, @PathVariable long friendId) {
         service.addToFriendsList(id, friendId);
     }
+
     @DeleteMapping("/{id}/friends/{friendId}")
     public void removeFriend(@PathVariable long id, @PathVariable long friendId) {
         service.deleteFromFriendsList(id, friendId);
     }
+
     @GetMapping("/{id}/friends")
     public List<User> getFriends(@PathVariable long id) {
         return service.getFriends(id);
     }
+
     @GetMapping("/{id}/friends/common/{otherId}")
     public List<User> getCommonFriends(@PathVariable long id, @PathVariable long otherId) {
         return service.getCommonFriends(id, otherId);
