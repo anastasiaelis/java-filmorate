6b3de2530faa188afc38640f077e9e3a5012fc5e
anastasiaelis <anastasia_elis@mail.ru>
2024-02-18 15:17:01 +0500

    new branch

--- src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryUserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryUserStorage.java
@@ -1,25 +1,16 @@
 package ru.yandex.practicum.filmorate.storage;
-
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.exception.UserNotFoundException;
 import ru.yandex.practicum.filmorate.model.User;
-
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-/*
-Класс-хранилище пользователей, имплементирующий интерфейс UserStorage
-*/
-
 @Slf4j
 @Component
 public class InMemoryUserStorage implements UserStorage {
-
     private final Map<Long, User> users = new HashMap<>();
     private long id = 0;
-
     @Override
     public User create(User user) {
         user.setId(++id);
@@ -27,19 +18,16 @@ public class InMemoryUserStorage implements UserStorage {
         log.info("Пользователь {} был создан", user.getId());
         return user;
     }
-
     @Override
     public User update(User user) {
         users.put(user.getId(), user);
         log.info("Пользователь {} был обновлен", user.getId());
         return user;
     }
-
     @Override
     public List<User> get() {
         return List.copyOf(users.values());
     }
-
     @Override
     public User getUserById(long id) {
         if (!users.containsKey(id)) {
