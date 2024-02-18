683b230f3ed32bb3c057c099f3eec2e5ed10f9fb
anastasiaelis <anastasia_elis@mail.ru>
2024-02-18 22:05:38 +0500

    new branch2

--- src/main/java/ru/yandex/practicum/filmorate/controller/Validator.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/Validator.java
@@ -8,22 +8,22 @@ import java.time.Month;
 @Slf4j
 public class Validator {
     public static void validateFilm(Film film) {
-        if (film.getName() == null || film.getName().isBlank()) {
+        /*if (film.getName() == null || film.getName().isBlank()) {
             log.error("Название фильма пустое");
             throw new ValidationException("Название фильма не может быть пустым");
         }
         if (film.getDescription().length() > 200) {
             log.error("Превышена максимальная длина описания фильма");
             throw new ValidationException("Описание фильма не может превышать 200 символов");
-        }
+        }*/
         if (film.getReleaseDate().isBefore(LocalDate.of(1895, Month.DECEMBER, 28))) {
             log.error("Дата релиза фильма раньше допустимого порога");
             throw new ValidationException("Дата релиза не должна быть ранее 28.12.1895");
         }
-        if (film.getDuration() <= 0) {
+       /* if (film.getDuration() <= 0) {
             log.error("Продолжительность фильма - не положительное число");
             throw new ValidationException("Продолжительность фильма должна быть больше нуля");
-        }
+        }*/
     }
 
     public static void validateUser(User user) {
