11c7ba8ad45908e75cfe1a68a4eb961130221a88
anastasiaelis <anastasia_elis@mail.ru>
2024-02-18 21:39:29 +0500

    new branch2

--- src/main/java/ru/yandex/practicum/filmorate/controller/Validator.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/Validator.java
@@ -8,29 +8,29 @@ import java.time.Month;
 @Slf4j
 public class Validator {
     public static void validateFilm(Film film) {
-        if (film.getName() == null || film.getName().isBlank()) {
+       /* if (film.getName() == null || film.getName().isBlank()) {
             log.error("Название фильма пустое");
             throw new ValidationException("Название фильма не может быть пустым");
         }
         if (film.getDescription().length() > 200) {
             log.error("Превышена максимальная длина описания фильма");
             throw new ValidationException("Описание фильма не может превышать 200 символов");
-        }
+        }*/
         if (film.getReleaseDate().isBefore(LocalDate.of(1895, Month.DECEMBER, 28))) {
             log.error("Дата релиза фильма раньше допустимого порога");
             throw new ValidationException("Дата релиза не должна быть ранее 28.12.1895");
         }
-        if (film.getDuration() <= 0) {
+        /*if (film.getDuration() <= 0) {
             log.error("Продолжительность фильма - не положительное число");
             throw new ValidationException("Продолжительность фильма должна быть больше нуля");
-        }
+        }*/
     }
 
     public static void validateUser(User user) {
-        if (!user.getEmail().contains("@")) {
+        /*if (!user.getEmail().contains("@")) {
             log.error("Email пользователя пуст или не содержит @");
             throw new ValidationException("Email должен содержать @ и не быть пустым");
-        }
+        }*/
         if (user.getLogin().isBlank() || user.getLogin().contains(" ")) {
             log.error("Логин пользователя пуст или содержит пробелы");
             throw new ValidationException("Логин не должен быть пустым или содержать пробелы");
