08387798df89787cbbabbdb9d7c56546500f237b
anastasiaelis <anastasia_elis@mail.ru>
2024-02-26 21:38:05 +0500

    commit

--- src/main/java/ru/yandex/practicum/filmorate/storage/UserDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserDbStorage.java
@@ -51,19 +51,15 @@ public class UserDbStorage implements UserStorage {
         return getUserById(user.getId());
     }
 
-   // @Override
-    public User addFriend(User user1,User user2) {
+    // @Override
+    public User addFriend(User user1, User user2) {
         getUserById(user1.getId());
-        String  sqlQuery = "insert into friend (user_id, friend_id) values (?, ?)";
-        long id1=user1.getId();
+        String sqlQuery = "insert into friend (user_id, friend_id) values (?, ?)";
+        long id1 = user1.getId();
         jdbcTemplate.update(sqlQuery, id1, user2.getId());
         log.info("Пользователь {} был обновлен", user1.getId());
         return getUserById(user1.getId());
     }
-
-
-
-
 
 
     @Override
