d4a6c371b0227478b32b5e9e84cadf95d83ba81f
anastasiaelis <anastasia_elis@mail.ru>
2024-02-28 21:51:59 +0500

    commit

--- src/main/java/ru/yandex/practicum/filmorate/storage/FriendshipDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FriendshipDbStorage.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate.storage;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
-import ru.yandex.practicum.filmorate.storage.friendship.FriendshipStorage;
 
 import java.util.List;
 
