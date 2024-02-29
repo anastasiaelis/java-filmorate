10075358a985db2df9f1d4d51b53d4bf2bedc667
anastasiaelis <anastasia_elis@mail.ru>
2024-02-29 21:11:00 +0500

    Новый тест

---
+++ src/test/java/ru/yandex/practicum/filmorate/UserDbStorageTest.java
@@ -0,0 +1,37 @@
+package ru.yandex.practicum.filmorate;
+
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.storage.UserDbStorage;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@JdbcTest // указываем, о необходимости подготовить бины для работы с БД
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
+class UserDbStorageTest {
+    private final JdbcTemplate jdbcTemplate;
+
+    @Test
+    public void testFindUserById() {
+        // Подготавливаем данные для теста
+        User newUser = new User(2, "user@email.ru", "vanya123", "Ivan Petrov", LocalDate.of(1990, 1, 1), new HashSet<>());
+        UserDbStorage userStorage = new UserDbStorage(jdbcTemplate);
+        userStorage.create(newUser);
+
+        // вызываем тестируемый метод
+        User savedUser = userStorage.getUserById(1);
+
+        // проверяем утверждения
+        assertThat(userStorage.create(savedUser))
+                .isNotNull() // проверяем, что объект не равен null
+                .usingRecursiveComparison() // проверяем, что значения полей нового
+                .isEqualTo(newUser);        // и сохраненного пользователя - совпадают
+    }
+}
