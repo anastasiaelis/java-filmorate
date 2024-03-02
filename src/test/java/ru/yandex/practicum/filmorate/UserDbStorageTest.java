2e30c965e084e839e7dad51d67ad5684b7cfcca7
anastasiaelis <anastasia_elis@mail.ru>
2024-03-02 22:42:02 +0500

    Почему то не проходит тест

--- src/test/java/ru/yandex/practicum/filmorate/UserDbStorageTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/UserDbStorageTest.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import ru.yandex.practicum.filmorate.model.User;
-import ru.yandex.practicum.filmorate.storage.UserDbStorage;
+//import ru.yandex.practicum.filmorate.storage.UserDbStorage;
 
 import java.time.LocalDate;
 import java.util.HashSet;
