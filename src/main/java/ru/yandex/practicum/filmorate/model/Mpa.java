4d87eda4b06dbd0063f66ce29df442580bc4c885
anastasiaelis <anastasia_elis@mail.ru>
2024-03-02 20:32:16 +0500

    mpa@validated

--- src/main/java/ru/yandex/practicum/filmorate/model/Mpa.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Mpa.java
@@ -2,21 +2,39 @@ package ru.yandex.practicum.filmorate.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import java.util.Objects;
 
 @Data
 @AllArgsConstructor
 
 public class Mpa {
+    @Getter
     @NotBlank
     @NotNull
-
-    private int id;
+    @Positive
+    private Integer id;
+    @NotBlank
     private String name;
 
     public Mpa() {
 
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Mpa)) return false;
+        Mpa mpa = (Mpa) o;
+        return id.equals(mpa.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
