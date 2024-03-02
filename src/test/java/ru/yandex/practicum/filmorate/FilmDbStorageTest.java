6f471e2413d03ad13a57ccc7ea623781b03d239c
anastasiaelis <anastasia_elis@mail.ru>
2024-03-02 21:42:20 +0500

    ordermpa

--- src/test/java/ru/yandex/practicum/filmorate/FilmDbStorageTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmDbStorageTest.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.model.Mpa;
 import ru.yandex.practicum.filmorate.storage.FilmDbStorage;
 
