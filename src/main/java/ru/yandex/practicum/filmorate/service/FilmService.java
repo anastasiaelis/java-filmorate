3f55c568d500f534135c9634d9102af6d1c0593a
anastasiaelis <anastasia_elis@mail.ru>
2024-03-03 09:16:33 +0500

    убираю лайки в фильмах

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -8,7 +8,7 @@ import ru.yandex.practicum.filmorate.exception.FilmNotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.storage.FilmStorage;
 
-import java.util.Comparator;
+//import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
